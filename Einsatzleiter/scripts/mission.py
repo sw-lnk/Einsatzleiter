9b70829d58d5503fe318980b3491b35b34d310b9
Swen Lenk <swen.lenk@gmail.com>
2024-06-19 20:46:49 +0200

    Add function to mission script.

--- Einsatzleiter/scripts/mission.py
+++ Einsatzleiter/scripts/mission.py
@@ -18,8 +18,7 @@ alphabet = string.ascii_letters + string.digits
 
 # pip install django-extensions
 
-# python manage.py runscript script_name
-
+# python manage.py runscript -v3 script_name
 
 def get_mails() -> list[dict]:
     # Prüfe auf neue Alarmdepeche
@@ -30,33 +29,39 @@ def get_mails() -> list[dict]:
     imap_server = os.getenv("IMAP_SERVER")
     imap_port = os.getenv("IMAP_PORT")
 
-    smtp_server = os.getenv("SMTP_SERVER")
-    smtp_port = os.getenv("SMTP_PORT")
-
     list_mails = []
-    with MailBox(imap_server).login(email_address, email_password) as mailbox:
+    with MailBox(imap_server).login(email_address, email_password, imap_port) as mailbox:
         for msg in mailbox.fetch(AND(from_=email_leitstelle)):
             values = [x.strip() for x in msg.text.split(';')]
             keys = ["main_id", "key_word_short", "city", "ward", "street", "street_no", "fire_alarm_system", "object", "comment", "flashing", "key_word_long", "overview", "units", "time"]
             dic = dict(zip(keys, values))
             dic['date'] = msg.date
+            dic['subject'] = msg.subject
+            
+            dic['main_id'] = int(dic['main_id'])
+            
             list_mails.append(dic)
     
     return list_mails
 
-
-def new_mission(msg: dict):
-    instance = CustomUser
+def bot_user() -> CustomUser:
     try:
-        user = instance.objects.get(username="bot")
-    except instance.DoesNotExist:
-        user = instance.objects.create(
+        user = CustomUser.objects.get(username="bot")
+    except CustomUser.DoesNotExist:
+        user = CustomUser.objects.create(
             username="bot",
             password="".join(secrets.choice(alphabet) for _ in range(20)),
             email=os.getenv("EMAIL_ADRESSE"),
             first_name="auto",
             last_name="bot",
         )
+    return user
+
+def check_mission_excist(msg) -> bool:
+    return Mission.objects.filter(main_id=msg['main_id']).exists()
+
+def new_mission(msg: dict) -> None:
+    user = bot_user()    
 
     if False:
         Mission.objects.create(
@@ -66,23 +71,40 @@ def new_mission(msg: dict):
             author=user)
     else:
         Mission.objects.create(
-            main_id=int(msg['main_id']),
+            main_id=msg['main_id'],
             keyword=f"{msg['key_word_short']} - {msg['key_word_long']}",
             street=msg['street'],
             street_no=msg['street_no'],
             start=msg['date'],
             author=user)
 
+def update_mission_end(msg) -> None:
+    if not check_mission_excist(msg):
+        return
+    
+    mission = Mission.objects.get(main_id=msg['main_id'])
+    
+    if mission.end:
+        return
+    
+    mission.end = msg['date']
+    mission.status = mission.CLOSED
+    mission.save()
+
+def create_or_upate_mission(msg) -> None:
+    # print(msg['subject'])
+    if not check_mission_excist(msg):
+        # Neuen Einsatz anlegen wenn eine Alarmdepeche eingeht.
+        try:
+            new_mission(msg)
+        except: pass # Log-Eintrag erzeugen bei einem Error
+    elif ('Abschlußbericht' in msg['subject']):
+        # Einsatz aktualisieren wenn die Abschlussdepeche zugestellt wird.
+        update_mission_end(msg)
+
 def run():
-    all_mails = get_mails()
-    ids = []
-    for msg in all_mails:
-        if not msg['main_id'] in ids:
-            try:
-                new_mission(msg)
-                ids.append(msg['main_id'])
-            except:
-                continue
+    for msg in get_mails():
+        create_or_upate_mission(msg)
 
 if __name__ == "__main__":
-    get_mails()
+    print('Nothing will happen.')
