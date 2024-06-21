725179f7f45f4baaaf4a6490dbf5eb9037159f6b
Swen Lenk <swen.lenk@gmail.com>
2024-06-21 18:58:49 +0200

    update docker (compose) files

--- Einsatzleiter/entrypoint.sh
+++ Einsatzleiter/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/ash
 
 echo "Apply database migrations"
+
+rm -r users/migrations/
+python manage.py makemigrations users
 python manage.py migrate
+python manage.py migrate --run-syncdb
 
 exec "$@"
